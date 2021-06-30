06ed0c0ced976efccb922f5c94a5e08915f16282
Sarmouts <saradis.m06@gmail.com>
2021-06-30 18:24:10 +0300

    Add project files.

---
+++ For Loop/For Loop.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+		for (int i = 0; i <= 10; i++)
+		{
+		 
+			for (int a = 0;  a <= 10; a++)
+			{
+				cout << i;
+
+
+
+			}
+
+
+			cout << endl;
+
+		}
+
+	        
+
+
+
+
+}
