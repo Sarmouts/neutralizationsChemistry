909bc361766046ff23c6a598a8eaffcc4d427335
Sarmouts <saradis.m06@gmail.com>
2021-08-25 15:53:33 +0300

    GIT

--- For Loop/For Loop.cpp
+++ For Loop/For Loop.cpp
@@ -14,6 +14,19 @@ string replace(string str)
 	}
 	return final;
 }
+string parenthesis(string str, int v)
+{
+	string final = "(" + str + ")";
+	if (str == "OH" && v != 1)
+	{
+		return final;
+	}
+	else if (str.length() != 3 || v == 1)
+	{
+		return str;
+	}
+	return final;
+}
 int main()
 {
 	cout << "Welcome to MathFlare's early access neutralizations' calculator" << endl;
@@ -25,7 +38,7 @@ int main()
 		cout << "HxX + YOHv" << endl; // X kai Y einai ta stoixeia kai ta x,y,z,v einai to noumero tous
 		cout << "Enter x" << endl;
 		cin >> x;
-		cout << "Enter Element X (non-metals, if you have something like this CO3, please write it with this way (CO3) so that the answer will be clearer)" << endl;
+		cout << "Enter Element X (non-metals)" << endl;
 		cin >> X;
 		cout << "Enter Element Y (metals)" << endl;
 		cin >> Y;
@@ -45,14 +58,14 @@ int main()
 		{
 			if (x == v)
 			{
-				answer = "H" + to_string(x) + X + " + " + Y + "OH" + to_string(v) + " --> " + to_string(x) + "H2O + " + Y + X;
+				answer = "H" + to_string(x) + X + " + " + Y + parenthesis("OH", v) + to_string(v) + " --> " + to_string(x) + "H2O + " + Y + X;
 				cout << replace(answer) << endl;
 				//cout << "H" << x << X << y << " + " << Y << z << "OH" << v << " --> " << HO << "H2O + " << Y << X << endl;
 				cout << "======================================================================================" << endl;
 			}
 			else {
 				HO = v * x;
-				answer = to_string(v) + "H" + to_string(x) + X + " + " + to_string(x) + Y + "OH" + to_string(v) + " --> " + to_string(HO) + "H2O + " + Y + to_string(x) + X + to_string(v);
+				answer = to_string(v) + "H" + to_string(x) + X + " + " + to_string(x) + Y + parenthesis("OH", v) + to_string(v) + " --> " + to_string(HO) + "H2O + " + Y + to_string(x) + parenthesis(X, v) + to_string(v);
 				cout << replace(answer) << endl;
 				//cout << v << "H" << x << X << y << " + " << x << Y << z << "OH" << v << " --> " << HO << "H2O + " << Y << x << X << v << endl;
 				cout << "======================================================================================" << endl;
